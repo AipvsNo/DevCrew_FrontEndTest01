c836a15f5795434843f4522ae04ce1fe8888285c
AipvsNo <yorn5G_5G@hotmail.com>
2023-12-05 23:09:53 +0700

    fixed css

--- src/Apollo.js
+++ src/Apollo.js
@@ -3,38 +3,39 @@ import { useQuery } from '@apollo/client';
 import {getCountries} from './Query.js';
 import './index.css'
 
+let groupByContinent = {};
 
+function rendering(data){
+  data.countries.forEach(c => {
+    if (!groupByContinent[c.continent.name]) {
+      groupByContinent[c.continent.name] = [];
+    }
+    else{
+      groupByContinent[c.continent.name].push(c);
+    }
+  });
+}
 
 const ShowallCountries =() =>{
   const {loading, error, data } = useQuery(getCountries);
-
   if (loading) return;
   if (error) return;
-
-  let groupByContinent = {};
-  data.countries.forEach(country => {
-    let continentname = country.continent.name;
-    if (!groupByContinent[continentname]) {
-      groupByContinent[continentname] = [];
-    }
-    groupByContinent[continentname].push(country);
-  });
-
+  rendering(data);
   return (
     <div>
-      <h1>Country List</h1>
-      {Object.entries(groupByContinent).map(([continent, countriesInContinent]) => (
-        <div key={continent}>
+      <h1>Around The World</h1>
+      {Object.entries(groupByContinent).map(([continent, index]) => (
+        <div>
           <h2>{continent}</h2>
           <tr>
             <th>Country Name</th>
             <th>Emoji</th>
           </tr>
           <tbody>
-            {countriesInContinent.map(country => (
+            {index.map(c => (
               <tr>
-                <td>{country.name}</td>
-                <td>{country.emoji}</td>
+                <td>{c.name}</td>
+                <td>{c.emoji}</td>
               </tr>
             ))}
           </tbody>
